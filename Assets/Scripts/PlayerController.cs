ef259c52c65eec5e156ba2586c950aec4227cf2e
Juan Esteban Gonzalez Sanchez <juanesgs2002@gmail.com>
2023-08-05 14:30:23 -0500

    Player animations created. Implemented blen trees for the animations. Set animation code for movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,21 +6,49 @@ public class PlayerController : MonoBehaviour{
 
     [SerializeField] float speed = 4f;
 
+    bool isWalking = false;
+    public Vector2 lastMovement = Vector2.zero;
+
     const string horizontal = "Horizontal";
     const string vertical = "Vertical";
+    const string lastHorizontal = "LastHorizontal";
+    const string lastVertical = "LastVertical";
+    const string walking = "IsMoving";
+
+    Animator playerAnimator;
+
+    private void Start() {
+        
+        playerAnimator = GetComponent<Animator>();
+    }
 
     void Update() {
         
+        isWalking = false;
+
         if(Mathf.Abs(Input.GetAxisRaw(horizontal)) > 0.5f){
 
             this.transform.Translate(
                 new Vector3(Input.GetAxisRaw(horizontal) * speed * Time.deltaTime, 0, 0));
+            isWalking = true;
+            lastMovement = new Vector2(Input.GetAxisRaw(horizontal), 0);
         }
 
         if(Mathf.Abs(Input.GetAxisRaw(vertical)) > 0.5f){
 
             this.transform.Translate(
                 new Vector3(0, Input.GetAxisRaw(vertical) * speed * Time.deltaTime, 0));
+            isWalking = true;
+            lastMovement = new Vector2(0, Input.GetAxisRaw(vertical));
         }
+
+        //Set animation states
+        playerAnimator.SetFloat(horizontal, Input.GetAxisRaw(horizontal));
+        playerAnimator.SetFloat(vertical, Input.GetAxisRaw(vertical));
+
+        playerAnimator.SetFloat(lastHorizontal, lastMovement.x);
+        playerAnimator.SetFloat(lastVertical, lastMovement.y);
+
+        playerAnimator.SetBool(walking, isWalking);
     }
 }
